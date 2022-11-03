b2c65b90095d80e9c6f5c553bcaa6816935947d8
Naufal Zaidan Nabhan <18119005@std.stei.itb.ac.id>
2022-11-03 07:42:17 +0700

    No 1 finished

--- No1.m
+++ No1.m
@@ -1,9 +1,27 @@
 close all;clear all;clc;
 
-SNRdB = 0:1:10 ;
+numberbits = 10^5
+number_simulation = 5;
+SNRdB=0:2:8
 SNR=10.^(SNRdB/10);
 
-BER = 0.5 * erfc(sqrt(SNR));
+for i=1:length(SNR)
+    for j=1:number_simulation
+        datatx = randi([0,1],1,numberbits);
+        bpsk = 2*datatx-1;
+        N = sqrt(0.5*1/SNR(i))*randn(1,numberbits);
+        y = bpsk+N;
+        datarx=heaviside(sign(y));
+        t= biterr (datatx,datarx);
+    end
+    BERsim(i)= t/numberbits;
+    
+end
 
-semilogy (SNRdB, BER,'m-<', 'linewidth' ,2.0);
+BERth = 0.5 * erfc(sqrt(SNR));
+
+semilogy (SNRdB, BERth,'m-<', 'linewidth' ,2.0);
+hold on
+semilogy (SNRdB, BERsim,'b-','linewidth',2.0);
+legend("BER Theoretical","BER Simulated");
 
