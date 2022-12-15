a2d6fed482569486483bcae354dfc20559a8b3bd
Naufal Zaidan Nabhan <18119005@std.stei.itb.ac.id>
2022-12-15 13:34:19 +0700

    Task finished

--- No2.m
+++ No2.m
@@ -1,40 +1,43 @@
 close all;clear all;clc;
+%Task 2 Jaringan Akses Nirkabel
+%Created by : Naufal Zaidan Nabhan / 18119005
 
-numberbits = 10^5;
-SNRdB=0:5:25
-SNR=10.^(SNRdB/10);
-numbersimulation = 5;
+%% Initialization
 
+numberbits = 10^5; %Number of bits 
+fd = 30; %Doppler frequency
+bitrate = 64000; %Bit rate
+SNRdB=0:5:25; %Signal-to-noise ratio in dB
+SNR=10.^(SNRdB/10); %Signal-to-noise ratio in linear
+numbersimulation = 5; %Number of monte carlo simulations
+
+%% Fading channel simulation
+
+%Transmitter
 datatx = randi([0,1],1,numberbits);
 bpsk = 2*datatx-1;
-
+%SImulation
 for i=1:length(SNRdB)
     BERsim = 0;
-    count = 0;
-    for k=1:numbersimulation
-         h = fading2(numberbits, 30, 1/128000);
-         n = 1/sqrt(2)*[randn(1,numberbits) + 1i*randn(1,numberbits)];
-         fadingchannel = bpsk.*h+10^(-SNRdB(i)/20)*n;
-         %receiver
+    for k=1:numbersimulation %Monte carlo simulation 
+         h = fading2(numberbits,fd, 1/bitrate); 
+         n = 1/sqrt(2)*[randn(1,numberbits) + 1i*randn(1,numberbits)]; %White gaussian noise
+         fadingchannel = bpsk.*h+10^(-SNRdB(i)/20)*n; %Fading channel
+         %Receiver
          y=fadingchannel./h;
-         %demodulasi
-         datarx = real(y)>0;
-         %menghitung error
-         error= biterr(datatx,datarx);
-         BERsim=BERsim+error/numberbits;
-         count = count+1;
+         datarx = real(y)>0; %Receiver decision
+         error= biterr(datatx,datarx); %Counting error
+         BERsim=BERsim+error/numberbits; %Counting BER
     end
-    BERsimavg(i)=BERsim/count;
+    BERsimavg(i)=BERsim/numbersimulation; %Counting BER average from monte carlo simulation
 end
     
+BERth = (1/2)*(1-sqrt(SNR./(1+SNR))); %Counting BER from fading channel formula
 
-
-ber_fad_theory = (1/2)*(1-sqrt(SNR./(1+SNR)));
-
-
-semilogy (SNRdB, ber_fad_theory,'m-<', 'linewidth' ,2.0);
+%% BER Plot
+semilogy (SNRdB, BERth, '-m<',"marker", "<", 'LineWidth', 2);
 hold on;grid on;
-semilogy (SNRdB, BERsimavg,'b-','linewidth',2.0);
+semilogy (SNRdB, BERsimavg, '--b', "marker", "*", 'linewidth', 1.75);
 legend("BER Theoretical","BER Simulated");
 xlabel('Eb/Io (dB)') 
 ylabel('Bit Error Rate')
